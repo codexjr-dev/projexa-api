60b7157e6509389f7b9e8db43f13695771f0d23e
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2023-10-23 16:07:48 -0300

    refactor: changes function in project and link update routes

--- src/modules/Link/LinkRoutes.js
+++ src/modules/Link/LinkRoutes.js
@@ -2,13 +2,12 @@ const router = require("express").Router();
 const { save, findByEj, update, remove } = require("./LinkController");
 const {
   existentUser,
-  authorizedUser,
   isLeadership,
 } = require("@middlewares/auth");
 
 router.post("/link", isLeadership, save);
 router.get("/link", existentUser, findByEj);
-router.patch("/link/:id", authorizedUser, update);
+router.patch("/link/:id", isLeadership, update);
 router.delete("/link/:id", isLeadership, remove);
 
 module.exports = router;
--- src/modules/Project/ProjectRoutes.js
+++ src/modules/Project/ProjectRoutes.js
@@ -2,13 +2,12 @@ const router = require("express").Router();
 const { save, findByEj, update, remove } = require("./ProjectController");
 const {
   existentUser,
-  authorizedUser,
   isLeadership,
 } = require("@middlewares/auth");
 
 router.post("/project", isLeadership, save);
 router.get("/project", existentUser, findByEj);
-router.patch("/project/:id", authorizedUser, update);
+router.patch("/project/:id", isLeadership, update);
 router.delete("/project/:id", isLeadership, remove);
 
 module.exports = router;
