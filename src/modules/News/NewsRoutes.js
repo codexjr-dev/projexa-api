207fd14aa0d67ecbf05709627856312f0a6f3bfa
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-11-07 12:11:37 -0300

    refactor: routes order changed

--- src/modules/News/NewsRoutes.js
+++ src/modules/News/NewsRoutes.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
-const { save, findByProject, update, remove, getAllNewsByEJ } = require("./NewsController");
+const { getAllNewsByEJ, findByProject, save, update, remove } = require("./NewsController");
 const {
   existentUser,
   isMemberOnProject,
   haveRightsToTheNews,
 } = require("@middlewares/auth");
 
-router.post("/news/:projectId", isMemberOnProject, save);
+router.get("/news", existentUser, getAllNewsByEJ);
 router.get("/news/:projectId", existentUser, findByProject);
+router.post("/news/:projectId", isMemberOnProject, save);
 router.patch("/news", haveRightsToTheNews, update);
 router.delete("/news/:projectId", haveRightsToTheNews, remove);
-
-router.get("/news", existentUser, getAllNewsByEJ);
 
 module.exports = router;
