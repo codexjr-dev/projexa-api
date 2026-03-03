f02e5477a8a8e330e7996824945c066d2d830129
Davi de Lucena Vasconcelos <davilucenana@gmail.com>
2026-03-03 17:40:44 -0300

    outro teste aparentemente funcional

--- test/integration/project.test.ts
+++ test/integration/project.test.ts
@@ -1,11 +1,11 @@
-import * as chai from 'chai';
-import chaiHttp, { request } from 'chai-http';
-import server from '../../src/server';
-import Project from '../../src/modules/project/project.model';
-import { startDatabase } from '../../src/config/config';
-import * as dotenv from 'dotenv';
-import data from '../datas/projects.data.json';
-import mainData from '../data.json';
+import * as chai from "chai";
+import chaiHttp, { request } from "chai-http";
+import server from "../../src/server";
+import Project from "../../src/modules/project/project.model";
+import { startDatabase } from "../../src/config/config";
+import * as dotenv from "dotenv";
+import data from "../datas/projects.data.json";
+import mainData from "../data.json";
 
 const HTTP_CODE = data.HTTP_CODE;
 const PROJECT_DEFAULT = data.projectDefault;
@@ -18,40 +18,52 @@ const organizationDefault = mainData.organizationDefault;
 
 chai.use(chaiHttp);
 
-describe('@Project', () => {
-    let EJ_MOCK: Record<string, string> = {};
+describe("@Project", () => {
+  let EJ_MOCK: Record<string, string> = {};
 
-    before(async () => {
-        dotenv.config();
-        await startDatabase();
+  before(async () => {
+    dotenv.config();
+    await startDatabase();
 
-        const ejResponse = await request.execute(server)
-            .post('/organization')
-            .send({ ...organizationDefault });
+    const ejResponse = await request
+      .execute(server)
+      .post("/organization")
+      .send({ ...organizationDefault });
 
-        EJ_MOCK = ejResponse.body.organization;
+    EJ_MOCK = ejResponse.body.organization;
 
-        const loginResponse = await request.execute(server)
-            .post('/signIn')
-            .send({
-                email: organizationDefault.presidentData.email,
-                password: organizationDefault.presidentData.password,
-            });
-
-        DEFAULT.token = loginResponse.body.dados.token;
-        DEFAULT.valid_id = loginResponse.body.dados.user._id;
+    const loginResponse = await request.execute(server).post("/signIn").send({
+      email: organizationDefault.presidentData.email,
+      password: organizationDefault.presidentData.password,
     });
 
-    describe('POST /project', () => {
+    DEFAULT.token = loginResponse.body.dados.token;
+    DEFAULT.valid_id = loginResponse.body.dados.user._id;
+  });
 
-        it('01. Tenta criar um projeto sem estar autorizado', async () => {
-            const response = await request.execute(server)
-                .post('/project')
-                .send({ ...PROJECT_DEFAULT });
+  describe("POST /project", () => {
 
-            chai.expect(response).to.have.status(HTTP_CODE.UNAUTHORIZED);
-            chai.expect(response.body).to.have.property('error');
-        });
+    // tá voltando 500, quando devia ser 401
+    it("01. Tenta criar um projeto sem estar autorizado", async () => {
+      const response = await request
+        .execute(server)
+        .post("/project")
+        .send({ ...PROJECT_DEFAULT });
 
+      chai.expect(response).to.have.status(HTTP_CODE.UNAUTHORIZED);
+      chai.expect(response.body).to.have.property("error");
     });
+
+    // tá voltando 500 quando devia ser 201. Aparentemente em auth.ts todo erro de autenticação retorna 500
+    it("Cria um projeto mas dessa vez com autenticação", async () => {
+      const response = await request
+        .execute(server)
+        .post("/project")
+        .set({ Authorization: `Bearer ${DEFAULT.token}` })
+        .send({ ...PROJECT_DEFAULT });
+
+      chai.expect(response).to.have.status(HTTP_CODE.CREATED);
+      chai.expect(response.body).to.have.property("project");
+    });
+  });
 });
